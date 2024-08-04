39c2b161ab388fa6d6c745d1b5b77b5d7e4344cb
GuBin <gubin@mindcoord.com>
2024-08-04 18:08:06 +0900

    feat: case 2

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
+
 app = Flask(__name__)
 
 CORS(app)
+
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'default_database_url')
+API_KEY = os.getenv('API_KEY', 'default_api_key')
 
 @app.route('/api/hello')
 def hello():
@@ -11,6 +21,13 @@ def hello():
 @app.route('/status')
 def hello_status():
     return jsonify(message="ok")
+
+@app.route('/api/env')
+def hello_env():
+    return jsonify(message="Hello .env", api_key=API_KEY, database_url=DATABASE_URL)
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3434)
